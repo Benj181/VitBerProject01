fixed wrong order in argymetns

opg3/opg-3b.py
@@ -15,13 +15,13 @@ def sawtoothPotential(x, alpha): # can only get values between -100 and 100
     
     
 
-alpha = 0.8
+alpha = 0.1
 T_p = 500 
-N_p = N_x * 12
+N_p = N_x * 1
 cycles = 10
 timeSteps = cycles * 2 * T_p
 
-Particles = [Particle(sawtoothPotential, i, startPos, alpha)
+Particles = [Particle(sawtoothPotential, i, alpha, startPos,)
               for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))]
 
 start = time.time()

opg3/opg-3c.py
@@ -23,7 +23,7 @@ averageCurrent = []
 T_pList = np.linspace(1, 1001, 50)
 start = time.time()
 for i, T_p in enumerate(T_pList): # Iterate over different timeintervals
-    Particles = [Particle(sawtoothPotential, i, startPos, alpha)
+    Particles = [Particle(sawtoothPotential, i, alpha, startPos)
               for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))]
     temp = time.time()
     # print(f"{i}: Simulating for {int(2*T_p)} as T_p")

opg3/opg-3d.py
@@ -20,10 +20,11 @@ N_p = N_x * 1 # 12
 
 normalizedParticleCurrent = []
 averageCurrent = []
-alphaList = np.linspace(0, 1, 50)
+alphaList = np.linspace(0, 1, 20) # 50
+# alphaList = [0.1, 0.5, 0.9]
 start = time.time()
 for i, alpha in enumerate(alphaList): # Iterate over different alphas
-    Particles = [Particle(sawtoothPotential, i, startPos, alpha)
+    Particles = [Particle(sawtoothPotential, i, alpha, startPos)
               for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))]
     temp = time.time()
     print(f"{i}: Simulating for {alpha} as alpha")
@@ -39,8 +40,8 @@ print(f"Total time: {round(time.time() - start, 2)} s")
 
 
 plt.plot(alphaList, averageCurrent)
-plt.xlabel("T_p")
+plt.xlabel("Alpha")
 plt.ylabel("Average current")
-plt.title("Average current over different T_p")
+plt.title("Average current over different Alpha")
 plt.grid()
 plt.show()