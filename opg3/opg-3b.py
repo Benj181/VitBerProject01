0a2ac225fd4bd319ae79a8dbfe5b6cd06f7f2a74
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-01-31 15:49:56 +0100

    fixed mistake. 3b should be good now

--- opg3/opg-3b.py
+++ opg3/opg-3b.py
@@ -16,7 +16,7 @@ def sawtoothPotential(x): # can only get values between -100 and 100
 
 alpha = 0.8
 T_p = 500 
-N_p = N_x * 1 # 12
+N_p = N_x * 12
 cycles = 10
 timeSteps = cycles * 2 * T_p
 
@@ -25,6 +25,7 @@ Particles = [Particle(sawtoothPotential, i, startPos)
 
 start = time.time()
 normalizedParticleCurrent = []
+    
 for i, _ in enumerate(range(timeSteps)): # Iterate over timesteps
     print(f"Simulating timestep {i-999}-{i+1} at time {round(time.time()-start, 3)} s") if (i + 1) % 1000 == 0 else None
     [particle.walkStep(T_p) for particle in Particles] # runs sim for every particle
@@ -32,8 +33,10 @@ for i, _ in enumerate(range(timeSteps)): # Iterate over timesteps
     normalizedParticleCurrent.append((movementCount[1] - movementCount[-1]) / N_p)
 
 print(f"Simulation took {round(time.time() - start, 2)} s with average time per particle {round((time.time()-start)*1000/N_p, 1)} ms")
-
 split_array = np.array_split(normalizedParticleCurrent, cycles)
 averageCurrent = [float(np.mean(part)) for part in split_array]
 for i, val in enumerate(averageCurrent):
     print(f"cycle {i+1}: {val:.2e}")  # Adjust decimal places as needed
+
+
+
