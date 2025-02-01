64cdbe5bdd4a4560a6fb8522d85cefc03c224f20
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-02-01 11:46:18 +0100

    added comments

--- opg3/opg-3d.py
+++ opg3/opg-3d.py
@@ -13,6 +13,7 @@ def sawtoothPotential(x, alpha): # can only get values between -100 and 100
     else:
         return False
     
+start = time.time()
 
 T_p = 500
 N_p = N_x * 1 # 12
@@ -21,19 +22,19 @@ N_p = N_x * 1 # 12
 normalizedParticleCurrent = []
 averageCurrent = []
 alphaList = np.linspace(0, 1, 10) # 50
-start = time.time()
+
 for i, alpha in enumerate(alphaList): # Iterate over different alphas
     Particles = [Particle(sawtoothPotential, i, alpha, startPos)
-              for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))]
-    temp = time.time()
+              for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))] # make list of particles
     print(f"{i}: Simulating for {round(alpha, 2)} as alpha")
+    temp = time.time()
     for i, _ in enumerate(range(int(2 * T_p ))): # Iterate over timesteps
         [particle.walkStep(T_p) for particle in Particles] # runs sim for every particle
         movementCount = Counter(particle.movement for particle in Particles)
-        normalizedParticleCurrent.append((movementCount[1] - movementCount[-1]) / N_p)
+        normalizedParticleCurrent.append((movementCount[1] - movementCount[-1]) / N_p) # calculate current
     print(f"Simulation took {round(time.time() - temp, 2)} s with average time per particle {round((time.time()-start)*1000/N_p, 1)} ms")
     print("-------------------------------------------")
-    averageCurrent.append(float(np.mean(normalizedParticleCurrent)))
+    averageCurrent.append(float(np.mean(normalizedParticleCurrent))) # avg current per alpha
 
 print(f"Total time: {round(time.time() - start, 2)} s")
 
