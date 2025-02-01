69b1f0f2559da459d235dc8dd8a8c1d036d85613
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-02-01 12:04:19 +0100

    added comments

--- opg3/opg-3a.py
+++ opg3/opg-3a.py
@@ -24,9 +24,9 @@ Particles = [Particle(sawtoothPotential, i, alpha) for i, _ in enumerate(range(N
 ParticlesAbsPos = [[] for _ in range(N_p)]
 for i, particle in enumerate(Particles): # simulates all particles
     ParticlesAbsPos[i] = [particle.walkStep(T_p) for _ in range(timeSteps)] # run walkstep [simulation] for all
+                                                                            # particles and store the absolute position    
 
-
-for i, Pos in enumerate(ParticlesAbsPos):
+for i, Pos in enumerate(ParticlesAbsPos): # plots each particles absolute position
     AbsolutePosAxis = np.linspace(0, len(Pos[::400]), len(Pos[::400]))
     plt.plot(AbsolutePosAxis, Pos[::400], markersize=1, label=f"Particle {i + 1}")
 
