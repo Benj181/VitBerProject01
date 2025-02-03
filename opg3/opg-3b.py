056a356e9665826a8a8055241907375100ba7a7a
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-02-03 10:25:50 +0100

    changed alpha back

--- opg3/opg-3b.py
+++ opg3/opg-3b.py
@@ -9,13 +9,13 @@ T_p = 500
 cycles = 10
 timeSteps = cycles * 2 * T_p
 
+    
+alpha = 0.8
 Particles = [Particle(sawtoothPotential, i, alpha, startPos)
               for i, startPos in enumerate(np.linspace(-N_x, N_x, N_p))]
 
 normalizedParticleCurrent = []
 
-    
-alpha = 0.8
 for i, _ in enumerate(range(timeSteps)): # Iterate over timesteps
     print(f"Simulating timestep {i-999}-{i+1} at time {round(time.time()-start, 3)} s") if (i + 1) % 1000 == 0 else None
     [particle.walkStep(T_p) for particle in Particles] # runs sim for every particle
